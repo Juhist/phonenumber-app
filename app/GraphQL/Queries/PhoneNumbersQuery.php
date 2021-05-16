43515acdf6b76d79977fbde303c2453bf3f5d9ba
Juhist <juhasz87istvan@gmail.com>
2021-05-16 09:42:53 +0200

    sorter&paginate in queries

--- app/GraphQL/Queries/PhoneNumbersQuery.php
+++ app/GraphQL/Queries/PhoneNumbersQuery.php
@@ -18,8 +18,47 @@ class PhoneNumbersQuery extends Query
         return Type::listOf(GraphQL::type('PhoneNumber'));
     }
 
+    public function args(): array
+    {
+        return [
+            'field' => [
+                'name' => 'field',
+                'type' => Type::string(),
+                'rules' => ['string']
+            ],
+            'sorter' => [
+                'name' => 'sorter',
+                'type' => Type::string(),
+                'rules' => ['in:ASC,DESC']
+            ],
+            'skip' => [
+                'name' => 'skip',
+                'type' => Type::int(),
+                'rules' => ['integer']
+            ],
+            'take' => [
+                'name' => 'take',
+                'type' => Type::int(),
+                'rules' => ['integer']
+            ],             
+        ];
+    }
+
     public function resolve($root, $args)
     {
-        return PhoneNumber::all();
+        $query = PhoneNumber::query();
+        if (isset($args['field']) && isset($args['sorter'])) {
+            if ($args['sorter'] == 'ASC') 
+            {
+                $query->orderBy($args['field']);
+            }
+            else {
+                $query->orderByDesc($args['field']);
+            }
+        }
+        if (isset($args['skip']) && isset($args['take'])) {
+            $query->skip($args['skip'])->take($args['take']);
+        }        
+        return $query->get();
     }
 }
--- app/GraphQL/Queries/UsersQuery.php
+++ app/GraphQL/Queries/UsersQuery.php
@@ -19,8 +19,48 @@ class UsersQuery extends Query
         return Type::listOf(GraphQL::type('User'));
     }
 
+    public function args(): array
+    {
+        return [
+            'field' => [
+                'name' => 'field',
+                'type' => Type::string(),
+                'rules' => ['string']
+            ],
+            'sorter' => [
+                'name' => 'sorter',
+                'type' => Type::string(),
+                'rules' => ['in:ASC,DESC']
+            ],
+            'skip' => [
+                'name' => 'skip',
+                'type' => Type::int(),
+                'rules' => ['integer']
+            ],
+            'take' => [
+                'name' => 'take',
+                'type' => Type::int(),
+                'rules' => ['integer']
+            ],                        
+        ];
+    }
+
     public function resolve($root, $args)
     {
-        return User::all();
+        $query = User::query();
+        if(isset($args['field']) && isset($args['sorter']))
+        {
+            if($args['sorter'] == 'ASC') 
+            {
+                $query->orderBy($args['field']);
+            }
+            else {
+                $query->orderByDesc($args['field']);
+            }
+        }
+        if (isset($args['skip']) && isset($args['take'])) {
+            $query->skip($args['skip'])->take($args['take']);
+        }
+        return $query->get();
     }
 }
